d09dc9981aa7dd79311dfddc1969e79eed215b12
Isaac Ayodeji <deejecks@live.com>
2014-11-25 03:29:47 +0000

    first commit

---
+++ javascript/app.js
@@ -0,0 +1,47 @@
+// problem: user interaction does not provide desired result
+// solution: add interactivity so the user can better manage shopping list
+
+
+//Add Item
+var addItem = function () {
+  //when the add button is pressed
+  //create new shopping item with the  text from the user entry
+    //input checkbox
+    //label
+    //input while we edit
+    //add button (button.edit)
+    //delete button (button.delete)
+    //each element needs to be modified and appended  
+}
+
+//Edit Pending 
+var editPending = function () {
+  //when the edit button is pressed
+    // if the class of the parent is .editMode
+      //switch from .editMode
+      //label text becomes the input's value
+    //else
+      //switch to .editMode
+      //input value becomes the label's text
+
+    //Toggle .editMode on the parent
+}
+
+//Delete an existing shopping item
+var deleteItem = function () {
+  //when the delete button is pressed
+    //remove the parent list item from the ul
+}
+
+//Mark a shopping item as purchased
+var markAsPurchased = function () {
+  //when the checkbox is checked
+    //append the shopping list item to purchased
+}
+
+//Mark a shopping item as Pending
+var markAsPending = function () {
+  //when the checkbox is unchecked
+    //append the task list item to the pending ul
+}
+
